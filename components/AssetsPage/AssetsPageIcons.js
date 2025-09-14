58b240127de95803b4d3df3f2ccd2ba18b8ec187
Gareth Evans <72853074+goliathguitartutorials@users.noreply.github.com>
2025-09-14 11:14:32 +0100

    Create AssetsPageIcons.js

---
+++ components/AssetsPage/AssetsPageIcons.js
@@ -0,0 +1,43 @@
+/*
+ * ==========================================================
+ * COMPONENT: Asset Page Icons
+ * PAGE: /assets
+ * FILE: /components/AssetsPage/AssetsPageIcons.js
+ * ==========================================================
+ */
+
+// A simple wrapper to handle sizing and styling for all icons
+const Icon = ({ children }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+    >
+        {children}
+    </svg>
+);
+
+export const ManageIcon = () => (
+    <Icon>
+        <rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect>
+    </Icon>
+);
+
+export const AddIcon = () => (
+    <Icon>
+        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="17 8 12 3 7 8"></polyline><line x1="12" y1="3" x2="12" y2="15"></line>
+    </Icon>
+);
+
+export const RefreshIcon = () => (
+    <Icon>
+        <polyline points="23 4 23 10 17 10"></polyline>
+        <path d="M20.49 15a9 9 0 1 1-2.12-9.36L23 10"></path>
+    </Icon>
+);
