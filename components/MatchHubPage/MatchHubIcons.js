6258d41de737b10048f6e2e13fe4417db0dd850a
Gareth Evans <72853074+goliathguitartutorials@users.noreply.github.com>
2025-09-30 15:11:41 +0100

    Update MatchHubIcons.js

--- components/MatchHubPage/MatchHubIcons.js
+++ components/MatchHubPage/MatchHubIcons.js
@@ -27,13 +27,9 @@ export const EditIcon = () => (
 );
 
 export const FootballIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
-        <line x1="12" y1="3" x2="12" y2="21"></line>
-        <circle cx="12" cy="12" r="3"></circle>
-        <path d="M7 3v2a2 2 0 0 1-2 2H3"></path>
-        <path d="M17 3v2a2 2 0 0 0 2 2h2"></path>
-        <path d="M7 21v-2a2 2 0 0 0-2-2H3"></path>
-        <path d="M17 21v-2a2 2 0 0 1 2-2h2"></path>
-    </svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <rect x="3" y="5" width="18" height="14" rx="2" ry="2"></rect>
+    <line x1="12" y1="5" x2="12" y2="19"></line>
+    <circle cx="12" cy="12" r="3"></circle>
+</svg>
 );
